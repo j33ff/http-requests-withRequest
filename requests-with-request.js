2cee602b74b55c6888376df9e7b5470002c15c32
Jeff FitzGerald <j33ff.fitzgerald@gmail.com>
2017-09-12 01:18:47 +0000

    adding response messages

--- requests-with-request.js
+++ requests-with-request.js
@@ -7,5 +7,7 @@ request.get('https://sytantris.github.io/http-examples/future.jpg')             
        })
        .on('response', function (response) {                           // Note 3
          console.log('Response Status Code: ', response.statusCode);
+         console.log('Response Status Message: ', response.statusMessage);
+         console.log('Content Type: ', response.headers['content-type']);
        })
-       .pipe(fs.createWriteStream('./downloaded.html'));
+       .pipe(fs.createWriteStream('./future.jpg'));
